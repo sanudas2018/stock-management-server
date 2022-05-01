d65f98e0b1a89eb8c218b43684ba943778bf7610
sanudas2018 <programing26@gmail.com>
2022-05-01 18:22:43 +0600

    mongo db connection

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require ('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -7,6 +8,18 @@ const app = express();
 //Middleware
 app.use(cors());
 app.use(express.json());
+//.......MongoDb add.....
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jz72n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("startech-bd").collection("product");
+  // perform actions on the collection object
+  console.log('mongo cannection')
+  client.close();
+});
+
 
 //...............
 app.get('/', (req, res) => {
@@ -19,3 +32,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
    console.log('star tec bd CMD = ', port);
 })
+
+
+// DB_USER=sanu2022
+// DB_PASSWORD=JroNiO2zBVKHlTE1
