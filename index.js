236cb9f8702abbb821a0143c13027d22f8dfe242
sanudas2018 <programing26@gmail.com>
2022-05-01 19:21:38 +0600

    service datal load

--- index.js
+++ index.js
@@ -13,12 +13,32 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jz72n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("startech-bd").collection("product");
-  // perform actions on the collection object
-  console.log('mongo cannection')
-  client.close();
-});
+// client.connect(err => {
+//   const collection = client.db("startech-bd").collection("product");
+//   // perform actions on the collection object
+//   console.log('mongo cannection')
+//   client.close();
+// });
+//.........
+async function run(){
+   try{
+      await client.connect();
+      const productCollection = client.db('startech-bd').collection('products');
+
+      //....mongodb(find multiple docu:). All data load.
+      app.get('/products', async(req, res) => {
+         const query = {}
+         const cursor = productCollection.find(query);
+         const products = await cursor.toArray();
+         res.send(products);
+      })
+
+
+   }finally{
+
+   }
+}
+run().catch(console.dir);
 
 
 //...............
