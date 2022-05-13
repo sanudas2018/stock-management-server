d8584081fadd2c8208ae6434068a2ae4ccf69045
sanudas2018 <programing26@gmail.com>
2022-05-13 18:44:43 +0600

    stock all manage

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 //.......MongoDb add.....
 
-
+// mongggggggggggg
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jz72n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 // client.connect(err => {
